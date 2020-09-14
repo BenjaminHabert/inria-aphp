27bd5e144cec17f3ed8a6dac61db7e3cdd39c03f
benjamin <benj.habert@gmail.com>
2020-09-14 18:24:36 +0200

    implement most of deduplication with unit tests

--- aphp/deduplicate.py
+++ aphp/deduplicate.py
@@ -1,3 +1,8 @@
+import pandas as pd
+import numpy as np
+import Levenshtein
+
+
 def detect_duplicates(df_patient):
     """Remove duplicates from Dataframe of patients
 
@@ -27,7 +32,14 @@ def detect_duplicates(df_patient):
 
 
 def _find_pairs_from_phone(df_patient):
+    """Return list of (patient_id_1, patient_id_2) for duplicated users (same phone and name)."""
     pairs = []
+    for _, group in df_patient.groupby("phone_number"):
+        if len(group) < 2:
+            continue
+        for patient_1, patient_2 in _iter_pairs_of_rows(group):
+            if _same_name(patient_1, patient_2):
+                pairs.append((patient_1["patient_id"], patient_2["patient_id"]))
     return pairs
 
 
@@ -43,13 +55,109 @@ def _deduplicate(df_patient, pairs_of_ids):
     return df_deduplicated
 
 
-def _build_id_groups(pairs_of_ids):
-    return {}
-
-
 def _add_id_groups(df_patient, groups_of_ids):
-    return df_patient
+    """Add `all_patient_ids` to df_patient.
+
+    Parameters
+    ----------
+        df_patient: DataFrame, same as `detect_duplicates()`
+        groups_of_ids: dict of sets; `patient_id -> {group_of_patient_ids}`
+
+    Returns
+    -------
+        DataFrame with additional column: `all_patient_ids` (set)
+
+    """
+    starting_dtype = df_patient["patient_id"].dtype
+
+    # build dataframe of patient_ids
+    records = (
+        {"patient_id": key, "all_patient_ids": value, "unique_patient_id": min(value)}
+        for key, value in groups_of_ids.items()
+    )
+    id_info = pd.DataFrame.from_records(records)
+    df = df_patient.merge(id_info, on="patient_id", how="left")
+
+    # if no match after merge (a line with duplicates), the set of all_patient_ids is just
+    # this patient_id
+    missing_id = df["all_patient_ids"].isnull()
+    df.loc[missing_id, "all_patient_ids"] = df.loc[missing_id, "patient_id"].transform(
+        lambda patient_id: {patient_id}
+    )
+
+    # otherwise (when it did match), we replace the patient_id by a unique_patient_id
+    # patient_id will now show the actual duplicates
+    df.loc[~missing_id, "patient_id"] = df.loc[~missing_id, "unique_patient_id"]
+    df["patient_id"] = df["patient_id"].astype(starting_dtype)
+
+    df = df.drop("unique_patient_id", axis="columns")
+    return df
 
 
 def _keep_one_line_per_patient(df_with_id_group):
-    return df_with_id_group
+    """Remove duplicates of `patient_id` while keeping the most frequent data for each duplicate."""
+    columns = df_with_id_group.columns
+
+    def keep_most_frequent(df):
+        if len(df) == 1:
+            return df.iloc[0]
+        s = {}
+        for col in columns:
+            values = df[col].value_counts()
+            if values.empty:
+                s[col] = np.NaN
+            else:
+                s[col] = values.index[0]
+
+        return pd.Series(s)
+
+    return df_with_id_group.groupby("patient_id", as_index=False).apply(
+        keep_most_frequent
+    )
+
+
+# Utility functions
+
+
+def _build_id_groups(pairs_of_ids):
+    """Create dict of id groups (as sets) from list of id pairs
+
+    Exemple
+    -------
+
+        >>> _build_id_groups([(1, 2), (2, 3), (8, 9)])
+        {
+            1: {1, 2, 3},
+            2: {1, 2, 3},
+            3: {1, 2, 3},
+            8: {8, 9},
+            9: {8, 0}
+        }
+
+    """
+    groups = {}
+    for (x, y) in pairs_of_ids:
+        xset = groups.get(x, set([x]))
+        yset = groups.get(y, set([y]))
+        jset = xset | yset
+        for z in jset:
+            groups[z] = jset
+    return groups
+
+
+def _iter_pairs_of_rows(df):
+    for i, (_, row_1) in enumerate(df.iterrows()):
+        for _, row_2 in df.iloc[i + 1 :].iterrows():  # noqa E203
+            yield (row_1, row_2)
+
+
+def _same_name(patient_1, patient_2, maximum_distance=2):
+    fullname_1 = _sorted_string(patient_1["given_name"], patient_1["surname"])
+    fullname_2 = _sorted_string(patient_2["given_name"], patient_2["surname"])
+    return Levenshtein.distance(fullname_1, fullname_2) <= maximum_distance
+
+
+def _sorted_string(*string_elements):
+    without_nans = (s if s and s is not np.NaN else "" for s in string_elements)
+    assembled = " ".join(without_nans)
+    return " ".join(sorted(s for s in assembled.split(" "))).strip()
---
+++ tests/test_deduplicate_main.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+
+from aphp import deduplicate
+
+
+def test_find_pairs_from_phone():
+    df = pd.DataFrame(
+        data=[
+            ("A", "benjamin", "habert", "phone_number_1"),
+            ("B", "bjamin", "habert", "phone_number_1"),  # same as A
+            ("C", "bjamin", "hert", "phone_number_1"),  # same as B (but not A)
+            ("D", "bjamin", np.NaN, "phone_number_1"),
+            ("E", "benjamin", "habert", "phone_number_2"),
+        ],
+        columns=["patient_id", "given_name", "surname", "phone_number"],
+    )
+    result = list(deduplicate._find_pairs_from_phone(df))
+    assert result == [("A", "B"), ("B", "C")]
+
+
+def test_add_id_groups():
+    df = pd.DataFrame(
+        data=[
+            ("A", "benjamin"),
+            ("B", "habert"),
+            ("C", "other"),
+        ],
+        columns=["patient_id", "some_column"],
+    )
+    groups_of_ids = {"A": {"A", "B"}, "B": {"A", "B"}}
+    expected_result = pd.DataFrame(
+        data=[
+            ("A", "benjamin", {"A", "B"}),
+            ("A", "habert", {"A", "B"}),
+            ("C", "other", {"C"}),
+        ],
+        columns=["patient_id", "some_column", "all_patient_ids"],
+    )
+    result = deduplicate._add_id_groups(df, groups_of_ids)
+
+    pd.testing.assert_frame_equal(result, expected_result)
+
+
+def test_keep_one_line_per_patient():
+    df = pd.DataFrame(
+        data=[
+            ("A", "bjamin", np.NaN, {"A", "B"}),
+            ("A", "benjamin", np.NaN, {"A", "B"}),
+            ("A", "benjamin", "test", {"A", "B"}),
+            ("C", "other", np.NaN, {"C"}),
+        ],
+        columns=["patient_id", "name", "col_with_nan", "all_patient_ids"],
+    )
+    expected_result = pd.DataFrame(
+        data=[
+            ("A", "benjamin", "test", {"A", "B"}),
+            ("C", "other", np.NaN, {"C"}),
+        ],
+        columns=["patient_id", "name", "col_with_nan", "all_patient_ids"],
+    )
+    result = deduplicate._keep_one_line_per_patient(df)
+    print(result)
+    pd.testing.assert_frame_equal(result, expected_result)
