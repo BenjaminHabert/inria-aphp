82019a896d87cbf39e307d40f752def47287a1bf
benjamin <benj.habert@gmail.com>
2020-09-14 14:12:15 +0200

    test that cleaning can run without errors

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,9 +1,16 @@
 from aphp import load
 
 
-def test_load_raw():
+def test_load_raw_patients():
     df = load.load_raw_patients()
     assert not df.empty
 
+
+def test_load_raw_tests():
     df = load.load_raw_tests()
+    assert not df.empty
+
+
+def test_load_clean_patients():
+    df = load.load_clean_patients()
     assert not df.empty
